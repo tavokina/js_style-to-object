500741cd24a7ea4729f48c909b132184257872d0
Anna Rodina <tavoshka@gmail.com>
2025-12-15 14:45:43 +0200

    Solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
   env: {
-    jest: true
+    jest: true,
   },
   rules: {
-    'no-proto': 0
+    'no-proto': 0,
   },
-  plugins: ['jest']
+  plugins: ['jest'],
+
+    "rules": {
+    "comma-dangle": ["error", "always-multiline"],
+    "function-paren-newline": "off"
+  },
+
 };
